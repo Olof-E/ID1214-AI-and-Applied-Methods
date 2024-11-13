d994c7123dd5c7dc3e88ca4023659dac6221f22c
Olof Eliasson <olof.eliasson1@gmail.com>
2024-11-13 22:25:37 +0100

    basic block data-struc & movement added

---
+++ blocks.py
@@ -0,0 +1,32 @@
+# relative positions of the blocks within a 4x4 matrix of each block
+# (0, 0) is the top left corner of the matrxi
+
+
+tBlock = [
+  [
+    (0, 2),
+    (1, 2),
+    (2, 2),
+    (1, 3),
+  ],
+  [
+    (0, 2),
+    (1, 1),
+    (1, 2),
+    (1, 3),
+  ],
+  [
+    (0, 3),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+  ],
+  [
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 2),
+  ],
+]
+
+blocks = [tBlock,]
--- tetris.py
+++ tetris.py
@@ -2,13 +2,17 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
-
+from blocks import blocks
 
 gameState = []
 playArea = (10, 20)
 screen = None
 clock = None
 running = False
+
+currBlock = None
+rotation = 0
+pos = (-1, -1)
 
 def main():
     init()
@@ -28,16 +32,44 @@ def init():
     print(gameState)
 
     pygame.init()
-    screen = pygame.display.set_mode((1080, 1080), pygame.DOUBLEBUF, 32)
+    screen = pygame.display.set_mode((1080, 720), pygame.DOUBLEBUF, 32)
     clock = pygame.time.Clock()
+    spawnBlock()
     running = True
     dt = 0
 
 
+def draw_rect_alpha(surface, color, rect, width):
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    pygame.draw.rect(shape_surf, color, shape_surf.get_rect(), width)
+    surface.blit(shape_surf, rect)
+
+def move():
+    pass
+
+def rotate():
+    pass
+
+def spawnBlock():
+    global currBlock, rotation, pos
+    
+    currBlock = blocks[0]
+    rotation = 0
+    pos = [3, 0]
+    
+def checkCollision():
+    pass
+    
+def updateState():
+    pos[1] += 1
+    pass
+
 def gameLoop():
     global screen, clock, running, dt
     
+    passedTime = 0    
     while running:
+        
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -47,15 +79,17 @@ def gameLoop():
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("black")
         
-        gameState[19][9] = 1
-        gameState[18][8] = 1
-        gameState[19][8] = 1
-        gameState[19][7] = 1
-        
-        
         for i in range(playArea[1]):
             for j in range(playArea[0]):
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255, 2), pygame.Rect((screen.get_width()-playArea[0]*32)/2.0+32*j, (screen.get_height()-playArea[1]*32)/2.0+32*i,32,32), width=not gameState[i][j])
+                draw_rect_alpha(screen, pygame.Color(255, 255*(not gameState[i][j]), 255*(not gameState[i][j]), 255 if gameState[i][j] else 40), pygame.Rect((screen.get_width()-playArea[0]*32)/2.0+32*j, (screen.get_height()-playArea[1]*32)/2.0+32*i,32,32), width=not gameState[i][j])
+        
+        for i in range(4):
+            rotatedBlock = currBlock[rotation % 90]            
+            topleft = ((screen.get_width()-playArea[0]*32)/2.0, (screen.get_height()-playArea[1]*32)/2.0)
+                
+            draw_rect_alpha(screen, pygame.Color(255, 0, 0, 255), pygame.Rect(topleft[0]+(pos[0]+rotatedBlock[i][0])*32, topleft[1]+(pos[1]+rotatedBlock[i][1])*32, 32, 32), 0)
+        
+        pygame.draw.rect(screen, (255,255,255), ((screen.get_width()-playArea[0]*32)/2.0, (screen.get_height()-playArea[1]*32)/2.0, playArea[0]*32, playArea[1]*32), width=2)
         # pygame.draw.circle(screen, "blue", player_pos, 40)
 
         # keys = pygame.key.get_pressed()
@@ -75,6 +109,11 @@ def gameLoop():
         # dt is delta time in seconds since last frame, used for framerate-
         # independent physics.
         dt = clock.tick(60) / 1000
+        
+        passedTime += dt
+        if passedTime > 0.5:
+            passedTime = 0
+            updateState()
 
 
 if __name__ == "__main__":
