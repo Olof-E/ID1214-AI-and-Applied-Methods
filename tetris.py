79d6e028f55ec777a670a4360921d85c5ea126d7
Olof Eliasson <olof.eliasson1@gmail.com>
2024-11-14 11:43:39 +0100

    line clearing & future block selection added

--- tetris.py
+++ tetris.py
@@ -2,6 +2,7 @@
 from math import fabs
 import os
 import random
+from turtle import clear
 
 from Vector2 import Vector2
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
@@ -27,7 +28,7 @@ def main():
 
 
 def init():
-    global screen, clock, running, dt
+    global screen, clock, running, dt, nextPieces
     print("Game Started")
 
     for i in range(playArea.y):
@@ -40,6 +41,10 @@ def init():
     pygame.init()
     screen = pygame.display.set_mode((1080, 720), pygame.DOUBLEBUF, 32)
     clock = pygame.time.Clock()
+    
+    nextPieces = list(range(len(blocks)))
+    random.shuffle(nextPieces)
+    
     spawnBlock()
     running = True
     dt = 0
@@ -58,12 +63,13 @@ def rotate():
 
 def spawnBlock():
     global nextPieces, currBlock, rotation, pos
+       
+    currBlock = blocks[nextPieces.pop(0)]
     
     if len(nextPieces) == 0:
         nextPieces = list(range(len(blocks)))
         random.shuffle(nextPieces)
-        
-    currBlock = blocks[nextPieces.pop()]
+    
     rotation = 0
     pos = Vector2(3, 0)
     
@@ -80,9 +86,24 @@ def checkCollision(pos):
         
     return 0
 
-def clearLines():
+def quickDrop():
+    currPiece = currBlock
+    while(currPiece == currBlock):
+        updateState()
     pass
 
+def clearLines():
+    global gameState
+    
+    cleared = []
+    for i in range(playArea.y):
+        if(sum(gameState[i]) == playArea.x):
+            cleared.append(i)
+            
+    gameState = [i for j, i in enumerate(gameState) if j not in cleared]
+    for i in range(len(cleared)):
+        gameState.insert(0, [0]*playArea.x)
+    
 def updateState():
     if(not checkCollision(Vector2(pos.x, pos.y+1))):
         pos.y += 1
@@ -113,6 +134,9 @@ def gameLoop():
                 elif event.key == pygame.K_d and not checkCollision(Vector2(pos.x + 1, pos.y)):
                     pos.x += 1
                 
+                if event.key == pygame.K_SPACE:
+                    quickDrop()
+                
                 if event.key ==  pygame.K_q:
                     rotation = (rotation + 90) % 360 
                 elif event.key ==  pygame.K_e:
@@ -121,18 +145,28 @@ def gameLoop():
         # fill the screen with a color to wipe away anything from last frame
         screen.fill((0, 0, 0))
         
+        topleft = Vector2((screen.get_width()-playArea.x*32)/2.0, (screen.get_height()-playArea.y*32)/2.0)
+    
         for i in range(playArea.y):
             for j in range(playArea.x):
                 draw_rect_alpha(screen, (255, 255*(not gameState[i][j]), 255*(not gameState[i][j]), 255 if gameState[i][j] else 40), pygame.Rect((screen.get_width()-playArea.x*32)/2.0+32*j, (screen.get_height()-playArea.y*32)/2.0+32*i,32,32), width=not gameState[i][j])
         
         for i in range(4):
             rotatedBlock = currBlock[rotation // 90]
-            topleft = ((screen.get_width()-playArea.x*32)/2.0, (screen.get_height()-playArea.y*32)/2.0)
                 
-            draw_rect_alpha(screen, (255, 0, 0, 255), pygame.Rect(topleft[0]+(pos.x+rotatedBlock[i].x)*32, topleft[1] + (pos.y + rotatedBlock[i].y)*32, 32, 32), 0)
+            draw_rect_alpha(screen, (255, 0, 0, 255), pygame.Rect(topleft.x+(pos.x+rotatedBlock[i].x)*32, topleft.y + (pos.y + rotatedBlock[i].y)*32, 32, 32), 0)
+        
+        # Draw next piece
+        pygame.draw.rect(screen, (255, 255, 255), (topleft.x+playArea.x*32 + 75, topleft.y, 150, 100), width=2)
+        for i in range(4):
+            if nextPieces:
+                rotatedBlock = blocks[nextPieces[0]][0]
+                
+                draw_rect_alpha(screen, (255, 0, 0, 255), pygame.Rect(topleft.x+playArea.x*32 + 96 + rotatedBlock[i].x*32, topleft.y - 32 + rotatedBlock[i].y*32, 32, 32), 0)
+        
         
         # Draw border
-        pygame.draw.rect(screen, (255,255,255), ((screen.get_width()-playArea.x*32)/2.0, (screen.get_height()-playArea.y*32)/2.0, playArea.x*32, playArea.y*32), width=2)
+        pygame.draw.rect(screen, (255,255,255), (topleft.x, topleft.y, playArea.x*32, playArea.y*32), width=2)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
@@ -141,7 +175,7 @@ def gameLoop():
         dt = clock.tick(60) / 1000
         
         passedTime += dt
-        if passedTime > 0.2:
+        if passedTime > 0.5:
             passedTime = 0
             updateState()
 
