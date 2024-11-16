a05dd5abb73cd8e8113ef44777d18816a96935e6
Olof-E <olof.eliasson1@gmail.com>
2024-11-16 14:19:51 +0100

    hold piece logic added

--- tetris.py
+++ tetris.py
@@ -16,10 +16,11 @@ clock = None
 running = False
 
 nextPieces = []
-nextColors = []
 
-currColor = None
-currBlock = None
+currBlock = -1
+heldPiece = -1
+heldAvailable = True
+
 rotation = 0
 pos = Vector2(3, -1)
 
@@ -42,14 +43,14 @@ def main():
 
 
 def init():
-    global screen, clock, running, dt, nextPieces, nextColors
+    global screen, clock, running, dt, nextPieces
     print("Game Started")
     
     gameState.clear()
     for i in range(playArea.y):
         gameState.append([])
         for _ in range(playArea.x):
-            gameState[i].append(0)
+            gameState[i].append(Vector2(0, -1))
     
     # print(gameState)
 
@@ -58,14 +59,8 @@ def init():
     clock = pygame.time.Clock()
     
     nextPieces = list(range(len(blocks)))
-    nextColors = list(range(len(colors)))
-    
-    rand = random.randbytes(10)
-    
-    random.seed(rand)
+        
     random.shuffle(nextPieces)
-    random.seed(rand)
-    random.shuffle(nextColors)
        
     spawnBlock()
         
@@ -85,20 +80,17 @@ def rotate():
     pass
 
 def spawnBlock():
-    global nextPieces, nextColors, currBlock, currColor, rotation, pos
+    global nextPieces, currBlock, rotation, pos
        
-    currBlock = blocks[nextPieces.pop(0)]
-    currColor = colors[nextColors.pop(0)]
+    currBlock = nextPieces.pop(0)
     
     if len(nextPieces) == 0:
         nextPieces = list(range(len(blocks)))
-        nextColors = list(range(len(colors)))
         random.shuffle(nextPieces)
-        random.shuffle(nextColors)
     
     rotation = 0
     pos = Vector2(3, -1)
-    if currBlock == blocks[len(blocks)-1]:
+    if blocks[currBlock] == blocks[len(blocks)-1]:
         pos = Vector2(3, 0)
         
     if(checkCollision(pos, rotation)):
@@ -106,13 +98,13 @@ def spawnBlock():
     
 def checkCollision(pos, rot):
     global currBlock, rotation
-    rotatedBlock = currBlock[rot // 90]
+    rotatedBlock = blocks[currBlock][rot // 90]
     for i in range(4):
         if (pos.x + rotatedBlock[i].x < 0 or pos.x + rotatedBlock[i].x >= playArea.x
             or pos.y + rotatedBlock[i].y >= playArea.y):
             return True
         
-        elif (gameState[pos.y+rotatedBlock[i].y][pos.x+rotatedBlock[i].x]):
+        elif (gameState[pos.y+rotatedBlock[i].y][pos.x+rotatedBlock[i].x].x):
             return True
         
     return 0
@@ -128,27 +120,30 @@ def clearLines():
     
     cleared = []
     for i in range(playArea.y):
-        if(sum(gameState[i]) == playArea.x):
+        if(sum(cell.x for cell in gameState[i]) == playArea.x):
             cleared.append(i)
             
     gameState = [row for idx, row in enumerate(gameState) if idx not in cleared]
     for i in range(len(cleared)):
-        gameState.insert(0, [0]*playArea.x)
+        gameState.insert(0, [Vector2(0, -1)]*playArea.x)
     
 def updateState():
+    global heldAvailable
+    
     if(not checkCollision(Vector2(pos.x, pos.y+1), rotation)):
         pos.y += 1
     else:
-        rotatedBlock = currBlock[rotation // 90]
+        rotatedBlock = blocks[currBlock][rotation // 90]
         for i in range(4):
-            gameState[pos.y+rotatedBlock[i].y][pos.x+rotatedBlock[i].x] = 1
+            gameState[pos.y+rotatedBlock[i].y][pos.x+rotatedBlock[i].x] = Vector2(1, currBlock)
 
         clearLines()
         spawnBlock()
+        heldAvailable = True
             
 
 def gameLoop():
-    global screen, clock, running, dt, currBlock, rotation, pos
+    global screen, clock, running, dt, currBlock, heldPiece, heldAvailable, rotation, pos
     
     passedTime = 0    
     while running:
@@ -164,10 +159,23 @@ def gameLoop():
                     pos.x -= 1
                 elif event.key == pygame.K_d and not checkCollision(Vector2(pos.x + 1, pos.y), rotation):
                     pos.x += 1
-                
-                if event.key == pygame.K_SPACE:
+                elif event.key == pygame.K_s:
                     quickDrop()
                 
+                if event.key == pygame.K_SPACE and heldAvailable:
+                    heldAvailable = False
+                    if heldPiece == -1:
+                        heldPiece = currBlock
+                        spawnBlock()
+                    else:
+                        temp = heldPiece
+                        heldPiece = currBlock
+                        currBlock = temp
+                        rotation = 0
+                        pos = Vector2(3, -1)
+                        if blocks[currBlock] == blocks[len(blocks)-1]:
+                            pos = Vector2(3, 0)
+                                        
                 if event.key ==  pygame.K_q and not checkCollision(Vector2(pos.x, pos.y), (rotation + 90) % 360):
                     rotation = (rotation + 90) % 360 
                 elif event.key ==  pygame.K_e and not checkCollision(Vector2(pos.x, pos.y), (rotation - 90) % 360):
@@ -180,12 +188,12 @@ def gameLoop():
     
         for i in range(playArea.y):
             for j in range(playArea.x):
-                draw_rect_alpha(screen, (255, 255*(not gameState[i][j]), 255*(not gameState[i][j]), 255 if gameState[i][j] else 40), pygame.Rect((screen.get_width()-playArea.x*32)/2.0+32*j, (screen.get_height()-playArea.y*32)/2.0+32*i,32,32), width=not gameState[i][j])
+                draw_rect_alpha(screen, colors[gameState[i][j].y] if gameState[i][j].x else (255, 255, 255, 40), pygame.Rect((screen.get_width()-playArea.x*32)/2.0+32*j, (screen.get_height()-playArea.y*32)/2.0+32*i,32,32), width=not gameState[i][j].x)
         
         for i in range(4):
-            rotatedBlock = currBlock[rotation // 90]
+            rotatedBlock = blocks[currBlock][rotation // 90]
                 
-            pygame.draw.rect(screen, currColor, pygame.Rect(topleft.x+(pos.x+rotatedBlock[i].x)*32, topleft.y + (pos.y + rotatedBlock[i].y)*32, 32, 32))
+            pygame.draw.rect(screen, colors[currBlock], pygame.Rect(topleft.x+(pos.x+rotatedBlock[i].x)*32, topleft.y + (pos.y + rotatedBlock[i].y)*32, 32, 32))
         
         # Draw next piece
         pygame.draw.rect(screen, WHITE, (topleft.x+playArea.x*32 + 75, topleft.y, 175, 125), width=2)
@@ -193,7 +201,7 @@ def gameLoop():
             if nextPieces:
                 rotatedBlock = blocks[nextPieces[0]][0]
                 
-                draw_rect_alpha(screen, colors[nextColors[0]], pygame.Rect(topleft.x+playArea.x*32 + 96 + rotatedBlock[i].x*32, topleft.y - 16 + rotatedBlock[i].y*32, 32, 32), 0)
+                draw_rect_alpha(screen, colors[nextPieces[0]], pygame.Rect(topleft.x+playArea.x*32 + 96 + rotatedBlock[i].x*32, topleft.y - 16 + rotatedBlock[i].y*32, 32, 32), 0)
         
         
         # Draw border
