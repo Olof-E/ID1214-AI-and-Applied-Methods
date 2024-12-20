f9812dfb24bcc711207f9f68171c049e2fad411a
Olof-E <olof.eliasson1@gmail.com>
2024-12-20 15:34:32 +0100

    basic state & action handling added

--- environment.py
+++ environment.py
@@ -22,17 +22,43 @@ from tf_agents.environments import wrappers
 from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
 
+# Observations
+# 0 - 9   |  column height in playing area
+# 10 - 11 |  x & y of current piece 
+# 12      |  rotation of current piece
+# 13      |  current piece
+# 14      |  next piece 
+# 15      |  held piece
+# 16      |  score
+# 17      |  died
+
+
+# Actions
+# 0  |  Move left
+# 1  |  Move Right
+# 2  |  Rotate CCW
+# 3  |  Rotate CW
+# 4  |  Quickdrop piece
+# 5  |  Switch held piece
+
 
 class TetrisEnvironment(py_environment.PyEnvironment):
-  def __init__(self):
-    self.tetris = Tetris()
-      
+  def __init__(self):      
     self._action_spec = array_spec.BoundedArraySpec(
-        shape=(), dtype=np.int32, minimum=0, maximum=4, name='action')
+        shape=(), dtype=np.int32, minimum=0, maximum=5, name='action')
     self._observation_spec = array_spec.BoundedArraySpec(
-        shape=(15,), dtype=np.int32, minimum=0, name='observation')
-    self._state = [0*15]
+        shape=(18,), dtype=np.int32, minimum=0, name='observation')
+    self._state = [0*18]
     self._episode_ended = False
+    
+    self.tetris = Tetris()
+    self.tetris.start()
+    
+    self._state[10] = self.tetris.pos.x
+    self._state[11] = self.tetris.pos.y
+    self._state[13] = self.tetris.currBlock
+    self._state[14] = self.tetris.nextPieces[0]
+
 
   def action_spec(self):
     return self._action_spec
@@ -43,6 +69,14 @@ class TetrisEnvironment(py_environment.PyEnvironment):
   def _reset(self):
     self._state = [0*15]
     self._episode_ended = False
+    
+    # TODO Reset Tetris board manually (Probably just call init again or something)
+    
+    self._state[10] = self.tetris.pos.x
+    self._state[11] = self.tetris.pos.y
+    self._state[13] = self.tetris.currBlock
+    self._state[14] = self.tetris.nextPieces[0]
+    
     return ts.restart(np.array([self._state], dtype=np.int32))
 
   def _step(self, action):
@@ -54,8 +88,22 @@ class TetrisEnvironment(py_environment.PyEnvironment):
 
     
     if action == 0:
+        self.tetris.move(-1)
+    elif action == 1:
+        self.tetris.move(1)
+    elif action == 2:
+        self.tetris.rotate(-1)
+    elif action == 3:
+        self.tetris.rotate(1)
+    elif action == 4:
+        self.tetris.quickDrop()
+    elif action == 5:
         pass
     
+    self._state[10] = self.tetris.pos.x
+    self._state[11] = self.tetris.pos.y
+    self._state[13] = self.tetris.currBlock
+    self._state[14] = self.tetris.nextPieces[0]
     
     
     if self._episode_ended or self._state >= 21:
